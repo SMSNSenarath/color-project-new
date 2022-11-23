e660980f3636711f0dbd1d2417ba07f1f5fbc707
Shakila Nishadini <smsnshakilaa372@gmail.com>
2022-11-23 15:24:20 +0530

    Fix Palette Route

--- src/App.js
+++ src/App.js
@@ -5,19 +5,41 @@ import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
   render() {
     // console.log(generatePalette(seedColors[4]));
     return (
       <Switch>
-        <Route exact path="/" render={()=> }/>
-        <Route exact path="/palette:id" render={()=> }/>
-        {/* <div className="App"> */}
-          {/* Passed individually using spread operator */}
-          {/* <Palette palette={generatePalette(seedColors[4])} />
-        </div> */}
+        <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE!</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
       </Switch>
     );
   }
 }
 
 export default App;
+
+{
+  /* <div className="App"> */
+}
+{
+  /* Passed individually using spread operator */
+}
+{
+  /* <Palette palette={generatePalette(seedColors[4])} />
+        </div> */
+}
