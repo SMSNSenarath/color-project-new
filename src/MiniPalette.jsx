037d14c399afccd6206cb5f208b1e5f5c957d6de
Shakila Nishadini <smsnshakilaa372@gmail.com>
2022-11-24 15:51:05 +0530

    Add Link to Palette

--- src/MiniPalette.jsx
+++ src/MiniPalette.jsx
@@ -18,7 +18,7 @@ const styles = {
     height: "150px",
     width: "100%",
     borderRadius: "5px",
-    overFlaw: "hidden",
+    overflow: "hidden",
   },
   title: {
     display: "flex",
@@ -55,7 +55,7 @@ function MiniPalette(props) {
     />
   ));
   return (
-    <div className={classes.root}>
+    <div className={classes.root} onClick={props.handleClick}>
       <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
